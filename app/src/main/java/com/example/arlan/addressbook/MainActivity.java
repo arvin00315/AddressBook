c5ef6cc56043e3d29e76042bd5cea817a4955ae9
Dummy <mydummy@gmail.com>
2017-07-25 12:40:30 +0800

    menu folder and menu.xml has been added to create UI menu

--- app/src/main/java/com/example/arlan/addressbook/MainActivity.java
+++ app/src/main/java/com/example/arlan/addressbook/MainActivity.java
@@ -3,6 +3,8 @@ package com.example.arlan.addressbook;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -21,6 +23,24 @@ public class MainActivity extends AppCompatActivity {
     Button mViewDB;
 
     @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.main_menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        super.onOptionsItemSelected(item);
+        switch(item.getItemId()){
+            case R.id.add_contact:
+                addContact();
+                break;
+        }
+
+        return true;
+    }
+
+    @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main); //  Main page upon running the apps
@@ -34,7 +54,7 @@ public class MainActivity extends AppCompatActivity {
         listView.setAdapter(recyclerAdapter);
     }
 
-    public void addContact(View view) {
+    /*public void addContact(View view) {
         String name = this.mName.getText().toString();
         int mobile = Integer.parseInt(this.mMobile.getText().toString());
         String email = this.mEmail.getText().toString();
@@ -47,6 +67,10 @@ public class MainActivity extends AppCompatActivity {
             Log.i("arvinsTag", "Insert Data Successful");
         }
 
+    }*/
+
+    public void addContact(){
+        Log.i("arvinsTag","add contact called");
     }
 
 
